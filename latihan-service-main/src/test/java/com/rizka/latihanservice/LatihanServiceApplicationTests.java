9b89b931dc6cbc12115cfb0973d108896cb4c04a
Rizka Annisa <113493757+RizkaAnnisa@users.noreply.github.com>
2023-01-25 18:01:53 -0800

    Add files via upload

---
+++ latihan-service-main/src/test/java/com/rizka/latihanservice/LatihanServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rizka.latihanservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LatihanServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
